5e179d212d9b4f74b209baeb61b726e619cd2f1e
01JoshC <j01joshc@byu.edu>
2025-10-22 22:37:28 -0600

    Made lots of changes! Working on trying to figure ut why exactly th incrmenet function is not working

--- src/read/read.jsx
+++ src/read/read.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
 
+function increment(){
+  localStorage.set(currentChapter, parseInt(localStorage.get(currentChapter))+1)
+  setDisplayBook(localStorage.getItem(currentBook))
+  setDisplayChapter(localStorage.getItem(currentChapter))
+  setDisplayText("Congratulations! You have clicked the next button.")
+}
+
 export function Read() {
+  const [displayText, setDisplayText] = React.useState('');
+  const [displayChapter, setDisplayChapter] = React.useState('');
+  const [displayBook, setDisplayBook] = React.useState('');
+
+  //database pretending
+  const currentChapter = "currentChapter";
+  const currentBook = "currentBook";
+
+  React.useEffect(() => {
+    if (!localStorage.getItem(currentChapter)){
+      localStorage.setItem(currentChapter, "1")
+      }
+
+    if (!localStorage.getItem(currentBook)){
+      localStorage.setItem(currentBook, "Genesis")
+      }
+
+    setDisplayBook(localStorage.getItem(currentBook))
+    setDisplayChapter(localStorage.getItem(currentChapter))
+    setDisplayText("1 In the beginning God created the heaven and the earth.")
+    }, []);
+
+
   return (
      <main>
       <div className="container-fluid px-7">
-        <h1>Genesis 1</h1>
-        <p>1 In the beginning God created the heaven and the earth.</p>
-        <p>2 And the earth was without form, and void; and darkness was upon the face of the deep. And the Spirit of God moved upon the face of the waters.</p>
-        <p>3 And God said, Let there be light: and there was light.</p>
+        <h1>{displayBook} {displayChapter}</h1>
+        <p>{displayText}</p>
       </div>
+    <button type="button" class="btn btn-secondary" onClick={() => increment()}>Next Chapter &#10145;</button>
     </main>
   );
 }
