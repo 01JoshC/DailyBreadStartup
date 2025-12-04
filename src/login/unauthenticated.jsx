1b50e4f4d535d708ae0b41be4de31db2d08c627b
01JoshC <j01joshc@byu.edu>
2025-12-03 18:04:28 -0700

    test to see if git is working

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-
 import Button from 'react-bootstrap/Button';
+import { MessageDialog } from './messageDialog';
 
 export function Unauthenticated(props) {
   const [userName, setUserName] = React.useState(props.userName);
   const [password, setPassword] = React.useState('');
+  const [displayError, setDisplayError] = React.useState(null);
+
+  async function loginUser() {
+    loginOrCreate(`/api/auth/login`);
+  }
+
+  async function createUser() {
+    loginOrCreate(`/api/auth/create`);
+  }
 
   async function loginOrCreate(endpoint) {
     const response = await fetch(endpoint, {
@@ -25,22 +34,27 @@ export function Unauthenticated(props) {
 
   return (
     <>
-    <div className="container-fluid">
-        <form method="get" action="read.html">
-            <div>
-              <label className="form-label"for="email">Email:</label>
-              <input className="form-control" type="text" placeholder="your@email.com" onChange={(e) => setUserName(e.target.value)}/>
-            </div>
-            <div>
-              <label className="form-label"for="password">Password:</label>
-              <input className="form-control" type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
-            </div>
-            <div className="container-fluid text-center">
-              <Button type="button" variant="light" onClick={() => loginOrCreate('/api/auth/login')} disabled={!userName || !password} >Login</Button>
-              <Button type="button" variant="light" onClick={() => loginOrCreate('/api/auth/create')} disabled={!userName || !password}>Create</Button>
-            </div>
-        </form> 
-    </div>
+      <div className="container-fluid">
+          <form method="get" action="read.html">
+              <div>
+                <label className="form-label" for="email">Email:</label>
+                <input className="form-control" type="text" placeholder="your@email.com" value={userName} onChange={(e) => setUserName(e.target.value)}/>
+              </div>
+              <div>
+                <label className="form-label" for="password">Password:</label>
+                <input className="form-control" type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
+              </div>
+              <div className="container-fluid text-center">
+                <Button variant='primary' onClick={() => loginUser()} disabled={!userName || !password}>
+                Login
+                </Button>
+                <Button variant='secondary' onClick={() => createUser()} disabled={!userName || !password}>
+                Create
+                </Button>
+              </div>
+          </form> 
+      </div>
+      <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
     </>
   );
 }
